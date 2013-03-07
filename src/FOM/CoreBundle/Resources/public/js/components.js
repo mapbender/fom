e5c9eb4a5f4de4ec7d2a3e3ad9f6c598ca81908d
Vadim Hermann <vhermann@wheregroup.com>
2013-03-07 17:04:21 +0100

    services designed; bugfixes; add components - description text, alt seperator; add clientbased item filter;

---
+++ src/FOM/CoreBundle/Resources/public/js/components.js
@@ -0,0 +1,28 @@
+$(function() {
+    // init tabcontainers
+    $(".tabContainer").find(".tab").bind("click", function(){
+        var me = $(this);
+        me.parent().parent().find(".active").removeClass("active");
+        me.addClass("active");
+        $("#" + me.attr("id").replace("tab", "container")).addClass("active");
+    });
+
+    // init filter inputs
+    $(".listFilterInput").bind("keyup", function(){
+        var me    = $(this);
+        var val   = me.val().trim();
+        var items = $("#" + me.attr("id").replace("input", "list")).find("li");
+    
+        if(val.length > 0){
+            var item = null;
+    
+            $.each(items, function(i, e){
+                item = $(e);
+                (item.text().toUpperCase().indexOf(val.toUpperCase()) >= 0) ? item.show() 
+                                                                            : item.hide();
+            });
+        }else{
+            items.show();
+        }
+    });
+});
