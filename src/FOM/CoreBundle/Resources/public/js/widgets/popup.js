c19a22b480340d40749578a66ac3f7390e6dd02f
Vadim Hermann <vhermann@wheregroup.com>
2013-04-04 18:23:36 +0200

    add latest jquery-ui; add custom popup widget;

---
+++ src/FOM/CoreBundle/Resources/public/js/widgets/popup.js
@@ -0,0 +1,219 @@
+(function($){
+  $.widget("mapbender.mbPopup", {
+    options: {},
+    buttons: [],
+    popup: null,
+
+    defaults: {
+      draggable:            true,
+      resizeable:           false, //not implemented yet
+      modal:                true,
+      showHeader:           true,
+      showCloseButton:      true,
+      cancelOnEsc:          true,
+      cancelOnOverlayClick: true,
+
+      width:                0, // 0 -> css
+      cssClass:             "",
+
+      title:                "",
+      subTitle:             "",
+      content:              "",
+      btnOkLabel:           "OK",
+      btnCancelLabel:       "Cancel",
+    },
+
+    _create: function(){},
+
+    showHint: function(customOptions){
+      var hintDefaults             = this.defaults;
+      hintDefaults.draggable       = false;
+      hintDefaults.resizeable      = false;
+      hintDefaults.showCloseButton = false;
+      hintDefaults.showHeader      = false;
+      hintDefaults.cssClass        = "hint";
+
+      this.options = $.extend(hintDefaults, customOptions);
+      this.addButton(this.options.btnOkLabel, "button")
+          ._createMarkup()
+          ._show();
+    },
+
+    showAjaxModal: function(customOptions, url, yesClick, beforeLoad, afterLoad){
+      var that = this;
+      var modalDefaults      = that.defaults;
+      modalDefaults.cssClass = "modal";
+
+      that.options = $.extend(modalDefaults, customOptions);
+      that.addButton(that.options.btnOkLabel, "button buttonOk", yesClick)
+          .addButton(that.options.btnCancelLabel, "button buttonCancel");
+
+      $.ajax({
+          url: url,
+          type: "POST",
+          beforeSend:  beforeLoad,
+          success: function(data){
+               that.options.content = data;
+               that._createMarkup()
+                   ._show();
+               afterLoad();
+          }
+      });
+    },
+
+    showModal: function(customOptions, yesClick){
+      with (this){
+        var modalDefaults      = defaults;
+        modalDefaults.cssClass = "modal";
+
+        options = $.extend(modalDefaults, customOptions);
+        addButton(options.btnOkLabel, null, yesClick)
+        .addButton(options.btnCancelLabel)
+        ._createMarkup()
+        ._show();
+      }
+    },
+
+    showCustom: function(customOptions){
+      this.options = $.extend(this.defaults, customOptions);
+      this._createMarkup()._show();
+    },
+
+    addButton: function(label, cssClass, clickFunction, append){
+      var that   = this;
+      cssClass   = (cssClass != undefined) ? 'class="' + cssClass + '"' : "";
+
+      var button = $('<a href="#" ' + cssClass + '>' + label + '</a>')
+                   .bind("click", 
+                         (typeof(clickFunction) == "function") ? 
+                         function(e){clickFunction.call(that, e)} :
+                         function(e){that.close.call(that, e)});
+
+      that.buttons.push(button);
+
+      return this;
+    },
+
+    close: function(){
+      var that = this;
+
+      that.popup.removeClass("show");
+      setTimeout(function() {that._destroy()},200);
+
+      return false;
+    },
+
+    _destroy: function(){
+      with (this){
+        popup.find(".popupButtons").find("*").unbind();
+        options = null;
+        popup.remove();
+        popup = null;
+        buttons = [];
+      }
+    },
+
+    _show: function(){
+      var that = this;
+      setTimeout(function() {that.popup.addClass("show")},10);
+    },
+
+    _getTemplate: function(){
+      return $('<div id="popupContainer" class="popupContainer">' +
+                 '<div id="popup" class="popup">' +
+                   '<div class="popupHead">' +
+                     '<span id="popupTitle" class="popupTitle"></span>' +
+                     '<span id="popupSubTitle" class="popupSubTitle"></span>' +
+                     '<span class="popupClose right">X</span>' +
+                   '</div>' +
+                   '<div id="popupContent" class="clear popupContent"></div>' +
+                   '<div class="popupButtons"></div>' +
+                   '<div class="clearContainer"></div>' +
+                 '</div>' +
+                 '<div class="overlay"></div>' +
+               '</div>');
+    },
+
+    _createMarkup: function(){
+      with (this){
+        popup = _getTemplate();
+
+        popup.addClass(this.options.cssClass)
+               .find(".popupTitle").text(options.title);
+        popup.find(".popupContent")
+               .append(options.content);
+
+        _removeCloseButton();
+        _removeHeader();
+        _removeOverlay();
+        _bindCancelEvents();
+        _addAllButtons();
+        _setWidth();
+
+        this.element.append(popup);
+
+        _bindDrag();
+      }
+      return this;
+    },
+
+    _setWidth: function(){
+      if(this.options.width > 0){
+        this.popup.find(".popup").css("width", this.options.width);
+      }
+    },
+
+    _removeCloseButton: function(){
+      if(!this.options.showCloseButton){
+        this.popup.find(".popupClose").remove();
+      }
+    },
+
+    _removeHeader: function(){
+      if(!this.options.showHeader){
+        this.popup.find(".popupHead").remove();
+      }
+    },
+
+    _removeOverlay: function(){
+      if(!this.options.modal){
+        this.popup.find(".overlay").remove();
+      }
+    },
+
+    _bindCancelEvents: function(){
+      var that = this;
+
+      if(this.options.cancelOnEsc){
+        $(document).one("keyup", function(e){
+          if(e.keyCode == 27) that.close();
+        });
+      }
+
+      if(this.options.cancelOnOverlayClick){
+        this.popup.find(".overlay").one("click", function(e){
+          that.close();
+        });
+      }
+    },
+
+    _bindDrag: function(){
+      if(this.options.draggable){
+        $("#popup").draggable();
+        $("#popup").draggable({ scroll: false });
+      }
+    },
+
+    _addAllButtons: function(){
+      if(this.buttons != null){
+        var len = this.buttons.length;
+        if(len > 0){
+          var btnContainer = this.popup.find(".popupButtons");
+          for(var i = 0; i < len; ++i){
+            btnContainer.append(this.buttons[i]);
+          }
+        }
+      }
+    }
+  });
+})(jQuery);
