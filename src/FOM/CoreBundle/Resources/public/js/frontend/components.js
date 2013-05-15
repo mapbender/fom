c258b62e0a23b90f6c9114c8604808b46f5200c7
Vadim Hermann <vhermann@wheregroup.com>
2013-05-15 16:54:01 +0200

    add new icons, designfixes

--- src/FOM/CoreBundle/Resources/public/js/frontend/components.js
+++ src/FOM/CoreBundle/Resources/public/js/frontend/components.js
@@ -4,6 +4,5 @@ $(function() {
         var parent = $(this).parent().parent();
         (parent.hasClass("opened")) ? parent.removeClass("opened") : parent.addClass("opened");
     }
-    console.log($(".toggleSideBar"))
     $(".toggleSideBar").bind("click", sideBarToggle);
 });
