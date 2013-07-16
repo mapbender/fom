860800095475b341712812d4f769c4b428d07b83
Paul Schmidt <paul.schmidt@wheregroup.com>
2013-07-16 17:40:05 +0200

    group transformer, type added

--- src/FOM/CoreBundle/Form/DataTransformer/GroupIdTransformer.php
+++ src/FOM/CoreBundle/Form/DataTransformer/GroupIdTransformer.php
@@ -2,11 +2,10 @@
 
 namespace FOM\CoreBundle\Form\DataTransformer;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Form\DataTransformerInterface;
 use Symfony\Component\Form\Exception\TransformationFailedException;
-use Doctrine\Common\Persistence\ObjectManager;
-
-
 use FOM\UserBundle\Entity\Group;
 
 /**
@@ -36,22 +35,35 @@ class GroupIdTransformer implements DataTransformerInterface
      */
     public function transform($id)
     {
-        if (!$id)
+        if(null === $id)
         {
             return null;
         }
-
-        $group = $this->om->getRepository('FOMUserBundle:Group')
-                ->findOneBy(array('id' => $id));
-
-        if (null === $group || !$group instanceof Group)
+        else if(is_string($id))
         {
-            throw new TransformationFailedException(sprintf(
-                    'A Group with id "%s" does not exist!', $id
-            ));
+            return $this->om->getRepository('FOMUserBundle:Group')
+                    ->findOneBy(array('id' => $id));
         }
-
-        return $group;
+        else if(is_array($id))
+        {
+            if(count($id) === 0)
+                return null;
+            $result = new ArrayCollection();
+            foreach($id as $value)
+            {
+                $group = $this->om->getRepository('FOMUserBundle:Group')
+                    ->findOneBy(array('id' => $value));
+                if($group !== null)
+                {
+                    $result->add($group);
+                }
+            }
+            return $result;
+        }
+        else
+        {
+            return null;
+        }
     }
 
     /**
@@ -63,8 +75,18 @@ class GroupIdTransformer implements DataTransformerInterface
      */
     public function reverseTransform($group)
     {
-        if (null === $group || !$group instanceof Group) return "";
-        else return (string) $group->getId();
+        if(null === $group) return "";
+        else if($group instanceof Group) return (string) $group->getId();
+        else if($group instanceof ArrayCollection)
+        {
+            $result = array();
+            foreach($group as $value)
+            {
+                $result[] = $value->getId();
+            }
+            return $result;
+        }
+        else return "";
     }
 
 }
