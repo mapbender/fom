304445f0680e7bb283c793d3af13647611b17aea
Christian Wygoda <christian.wygoda@wheregroup.com>
2013-08-12 17:19:52 +0200

    Form theme support for collection fields, fixes mapbender/mapbender#93

---
+++ src/FOM/CoreBundle/Resources/public/js/widgets/collection.js
@@ -0,0 +1,40 @@
+(function($) {
+
+$(document).on('click', '.collectionAdd', function(event) {
+    event.preventDefault();
+
+    // Gather all needed information, like
+    //  collection we're handling right now...
+    var collection = $(event.target).parent(),
+        // The prototype text for the new item...
+        prototype = collection.data('prototype'),
+        // The index, which might be undefined first...
+        index = collection.data('index') || 0,
+
+        // And finally parse the prototype into a new clean item for insertion.
+        item = $($.parseHTML(prototype
+            .trim()
+            .replace(/__name__/g, index))[0])
+            .addClass('collectionItem');
+
+    // Also add a delete button if needed
+    if(collection.data('allow-delete') == '1') {
+        item.append($('<a href="#delete" class="collectionRemove iconSmall iconRemove"></a>'));
+    }
+
+    // Now let's enter that item...
+    collection.append(item);
+    // And update our counter.
+    collection.data('index', index + 1);
+});
+
+$(document).on('click', '.collectionRemove', function(event) {
+    event.preventDefault();
+
+    // Get the item...
+    var item = $(event.target).closest('.collectionItem');
+    // And remove it.
+    item.remove();
+});
+
+})(jQuery);
