a0e668d802257312779b991e15769336427143bb
Paul Schmidt <paul.schmidt@wheregroup.com>
2013-09-04 14:29:15 +0200

    function getContent renamed to content

--- src/FOM/CoreBundle/Resources/public/js/widgets/popup2.js
+++ src/FOM/CoreBundle/Resources/public/js/widgets/popup2.js
@@ -216,7 +216,7 @@ var Mapbender = (function($, Mapbender) {
             var selfElement = this.$element;
 
             if(content) {
-                this.setContent(content);
+                this.content(content);
             }
 
             selfElement.trigger('open');
@@ -252,9 +252,11 @@ var Mapbender = (function($, Mapbender) {
          * @todo: How to do it right
          */
         destroy: function() {
-            this.$element.trigger('destroy');
-            this.$element.remove();
-            this.$element = null;
+            if(this.$element){
+                this.$element.trigger('destroy');
+                this.$element.remove();
+                this.$element = null;
+            }
         },
 
         /**
