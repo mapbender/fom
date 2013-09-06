54cf143abcc365d01a7772d01c81672e7bdc509c
Stefan Winkelmann <stefan.winkelmann@wheregroup.com>
2013-09-06 16:04:17 +0200

    components.js split to sidepane.js and tabcontainer.js

---
+++ src/FOM/CoreBundle/Resources/public/js/frontend/sidepane.js
@@ -0,0 +1,8 @@
+$(function() {
+    // init sidebar toggle -------------------------------------------------------------------
+    var sideBarToggle = function(){
+        var parent = $(this).parent().parent();
+        (parent.hasClass("opened")) ? parent.removeClass("opened") : parent.addClass("opened");
+    };
+    $(".toggleSideBar").bind("click", sideBarToggle);
+});
--- src/FOM/CoreBundle/Resources/public/js/frontend/tabcontainer.js
+++ src/FOM/CoreBundle/Resources/public/js/frontend/tabcontainer.js
@@ -1,11 +1,4 @@
 $(function() {
-    // init sidebar toggle -------------------------------------------------------------------
-    var sideBarToggle = function(){
-        var parent = $(this).parent().parent();
-        (parent.hasClass("opened")) ? parent.removeClass("opened") : parent.addClass("opened");
-    }
-    $(".toggleSideBar").bind("click", sideBarToggle);
-
     // init tabcontainers --------------------------------------------------------------------
     $(".tabContainer").on('click', '.tab', function() {
         var me = $(this);
