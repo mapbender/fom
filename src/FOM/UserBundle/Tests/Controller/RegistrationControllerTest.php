42085b9ed096e658b2149e457fe061c90b9ba8da
Christian Wygoda <info@wygoda.net>
2014-06-04 10:47:00 +0200

    Added comments to self registration tests

--- src/FOM/UserBundle/Tests/Controller/RegistrationControllerTest.php
+++ src/FOM/UserBundle/Tests/Controller/RegistrationControllerTest.php
@@ -6,11 +6,17 @@ use FOM\Component\Test\SharedApplicationWebTestCase;
 
 class RegistrationControllerTest extends SharedApplicationWebTestCase
 {
+    /**
+     * Test that self registration is disabled with the default configuration.
+     */
     public function testIndex() {
         $crawler = self::$client->request('GET', '/user/registration');
         $this->assertEquals(403, self::$client->getResponse()->getStatusCode());
     }
 
+    /**
+     * Test a full self registration workflow
+     */
     public function testSelfRegistration() {
         $client = static::createClient(array(
             'environment' => 'test_selfregister'
