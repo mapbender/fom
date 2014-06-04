5608530ad3b683e3991d62707d25eaab2baed3b6
Christian Wygoda <info@wygoda.net>
2014-06-04 10:42:56 +0200

    More self registration testing

--- src/FOM/UserBundle/Tests/Controller/RegistrationControllerTest.php
+++ src/FOM/UserBundle/Tests/Controller/RegistrationControllerTest.php
@@ -8,6 +8,15 @@ class RegistrationControllerTest extends SharedApplicationWebTestCase
 {
     public function testIndex() {
         $crawler = self::$client->request('GET', '/user/registration');
-        $this->assertEquals(self::$client->getResponse()->getStatusCode(), 403);
+        $this->assertEquals(403, self::$client->getResponse()->getStatusCode());
+    }
+
+    public function testSelfRegistration() {
+        $client = static::createClient(array(
+            'environment' => 'test_selfregister'
+        ));
+
+        $crawler = $client->request('GET', '/user/registration');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 }
