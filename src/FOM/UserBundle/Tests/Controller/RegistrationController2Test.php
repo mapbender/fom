6c2abbe9a51912530673cd74c51aabe62324b191
Christian Wygoda <info@wygoda.net>
2014-06-05 10:49:49 +0200

    Finished RegistrationController2Test

--- src/FOM/UserBundle/Tests/Controller/RegistrationController2Test.php
+++ src/FOM/UserBundle/Tests/Controller/RegistrationController2Test.php
@@ -6,6 +6,10 @@ use FOM\Component\Test\SharedApplicationWebTestCase;
 
 class RegistrationController2Test extends SharedApplicationWebTestCase
 {
+    protected static $username = 'testuser';
+    protected static $password = 'testpassword';
+    protected static $email = 'testuser@example.com';
+
     // Using a different config environment. Due to PHP's crazy late static
     // binding for properties, we have to redeclare $application and $client
     // as well... Oh well...
@@ -25,12 +29,11 @@ class RegistrationController2Test extends SharedApplicationWebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
         // Send form
-        $button = $crawler->filter('form button');
-        $form = $button->form(array(
-            'User[username]' => 'testuser',
-            'User[password][first]' => 'testingpass',
-            'User[password][second]' => 'testingpass',
-            'User[email]' => 'testuser@example.com',
+        $form = $crawler->filter('form button')->form(array(
+            'User[username]' => self::$username,
+            'User[password][first]' => self::$password,
+            'User[password][second]' => self::$password,
+            'User[email]' => self::$email,
         ));
         $client->submit($form);
         $this->assertContains($client->getResponse()->getStatusCode(), array(301, 302));
@@ -44,9 +47,37 @@ class RegistrationController2Test extends SharedApplicationWebTestCase
 
         // Asserting e-mail data
         $this->assertInstanceOf('Swift_Message', $message);
+        $this->assertEquals(self::$email, key($message->getTo()));
         $this->assertEquals(1, preg_match('/\/user\/activate\?token=(\w+)/',
             $message->getBody(), $matches));
         $token = $matches[1];
         // @todo: Go on with token
+
+        // Try to login without activating, must redirect back to login form
+        $crawler = $client->request('GET', '/user/login');
+        $form = $crawler->filter('form input[type="submit"]')->form(array(
+            '_username' => self::$username,
+            '_password' => self::$password));
+        $client->submit($form);
+
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\RedirectResponse', $client->getResponse());
+        $this->assertRegExp('/\/user\/login$/', $client->getResponse()->headers->get('location'));
+
+        // Activate using token
+        $crawler = $client->request('GET', '/user/activate?token=' . $token);
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\RedirectResponse', $client->getResponse());
+        $this->assertRegExp('/\/user\/registration\/done$/', $client->getResponse()->headers->get('location'));
+
+        // Login
+        $crawler = $client->request('GET', '/user/login');
+        $form = $crawler->filter('form input[type="submit"]')->form(array(
+            '_username' => self::$username,
+            '_password' => self::$password));
+        $client->submit($form);
+
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\RedirectResponse', $client->getResponse());
+        $securityCollector = $client->getProfile()->getCollector('security');
+        $this->assertEquals(true, $securityCollector->isAuthenticated());
+        $this->assertEquals(self::$username, $securityCollector->getUser());
     }
 }
