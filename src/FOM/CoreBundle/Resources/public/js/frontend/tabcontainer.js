b5f096d3f566120bc2074b2eef409ab74ac44b6f
Christian Wygoda <christian.wygoda@wheregroup.com>
2014-09-04 15:37:06 +0200

    Open accordions can be closed, too

--- src/FOM/CoreBundle/Resources/public/js/frontend/tabcontainer.js
+++ src/FOM/CoreBundle/Resources/public/js/frontend/tabcontainer.js
@@ -2,12 +2,18 @@ var initTabContainer = function($context){
     $(".tabContainer, .tabContainerAlt", $context).on('click', '.tab,.accordion', function(event) {
         var me = $(this);
         var tabcont = $(event.delegateTarget)
+        var wasActive = me.hasClass('active');
         tabcont.find(".active").removeClass("active");
-        me.addClass("active");
-        if (me.hasClass('tab')) {
+        if(me.hasClass('accordion')) {
+            if(wasActive) {
+                me.removeClass('active');
+            } else {
+                me.addClass('active');
+                $("#" + me.attr("id").replace("accordion", "container"), tabcont).addClass("active");
+            }
+        } else {
+            me.addClass("active");
             $("#" + me.attr("id").replace("tab", "container"), tabcont).addClass("active");
-        } else if (me.hasClass('accordion')) {
-            $("#" + me.attr("id").replace("accordion", "container"), tabcont).addClass("active");
         }
     });
 };
