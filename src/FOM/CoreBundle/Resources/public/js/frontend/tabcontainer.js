836515ca85d6939cd9d7a0c14353d78e64db6a9e
Paul Schmidt <paul.schmidt@wheregroup.com>
2014-10-17 17:45:05 +0200

    tab container css class changed

--- src/FOM/CoreBundle/Resources/public/js/frontend/tabcontainer.js
+++ src/FOM/CoreBundle/Resources/public/js/frontend/tabcontainer.js
@@ -1,26 +1,24 @@
-var initTabContainer = function($context){
-    $(".tabContainer, .tabContainerAlt", $context).on('click', '.tab,.accordion', function(event) {
+var initTabContainer = function ($context) {
+    $(".tabContainer, .tabContainerAlt").on('click', '.tab', function () {
+        var me = $(this);
+        me.parent().parent().find(".active").removeClass("active");
+        me.addClass("active");
+        $("#" + me.attr("id").replace("tab", "container")).addClass("active");
+    });
+    $(".accordionContainer", $context).on('click', '.accordion', function (event) {
         var me = $(this);
         var tabcont = $(event.delegateTarget)
         var wasActive = me.hasClass('active');
-        tabcont.find(".active").removeClass("active");
-        if(me.hasClass('accordion')) {
-            if(wasActive) {
-                me.removeClass('active');
-                me.parents(".tabContainer:first,.tabContainerAlt:first").addClass('noActive');
-            } else {
-                me.parents(".tabContainer:first,.tabContainerAlt:first").removeClass('noActive');
-                me.addClass('active');
-                $("#" + me.attr("id").replace("accordion", "container"), tabcont).addClass("active");
-            }
-        } else {
-            me.addClass("active");
-            $("#" + me.attr("id").replace("tab", "container"), tabcont).addClass("active");
+        if (!wasActive) {
+            tabcont.find(".active").removeClass("active");
+            me.parents(".tabContainer:first,.tabContainerAlt:first").removeClass('noActive');
+            me.addClass('active');
+            $("#" + me.attr("id").replace("accordion", "container"), tabcont).addClass("active");
         }
     });
 };
 
-$(function() {
+$(function () {
     // init tabcontainers --------------------------------------------------------------------
     initTabContainer($('body'));
 });
