edb1bc662a724f89eca6a0006438ccc8e298d0fc
Andriy Oblivantsev <eslider@gmail.com>
2014-11-04 15:42:26 +0100

    fix accordion scrolling area (IE bug)

--- src/FOM/CoreBundle/Resources/public/js/frontend/tabcontainer.js
+++ src/FOM/CoreBundle/Resources/public/js/frontend/tabcontainer.js
@@ -1,24 +1,60 @@
-var initTabContainer = function($context){
-    $(".tabContainer, .tabContainerAlt", $context).on('click', '.tab,.accordion', function(event) {
+var initTabContainer = function ($context) {
+
+    $(".tabContainer, .tabContainerAlt").on('click', '.tab', function () {
         var me = $(this);
-        var tabcont = $(event.delegateTarget)
-        var wasActive = me.hasClass('active');
-        tabcont.find(".active").removeClass("active");
+        me.parent().parent().find(".active").removeClass("active");
+        me.addClass("active");
+        $("#" + me.attr("id").replace("tab", "container")).addClass("active");
+    });
+
+    var accordion = $.extend($(".accordionContainer", $context), {
+        hasOutsideScroll: function() {
+            return this.parent().height() < this.height();
+        },
+        updateHeight:       function() { 
+            var contentCells = $("> .container-accordion > .accordion-cell", this);
+            var tabCells = $("> .accordion", this);
+            var tabCellsHeight = this.parent().height();
+
+            $.each(tabCells, function(idx, el) {
+                var tabCell = $(el);
+                tabCellsHeight -= tabCell.height();
+            });
+            contentCells.height(tabCellsHeight);
+        }
+});
+
+    // IE Scroll BugFix
+    if(accordion.hasOutsideScroll()){
+        accordion.updateHeight();
+        $(window).on('resize', $.proxy(accordion.updateHeight, accordion));
+    }
+
+    accordion.on('click', '.accordion', function(event) {
+        var me = $(this);
+        var tab = $(event.delegateTarget);
+        var isActive = me.hasClass('active');
+
+        if(isActive) {
+            return;
+        }
+
+        tab.find(".active").removeClass("active");
         if(me.hasClass('accordion')) {
-            if(wasActive) {
+            if(isActive) {
                 me.removeClass('active');
             } else {
                 me.addClass('active');
-                $("#" + me.attr("id").replace("accordion", "container"), tabcont).addClass("active");
+                $("#" + me.attr("id").replace("accordion", "container"), tab).addClass("active");
             }
         } else {
             me.addClass("active");
-            $("#" + me.attr("id").replace("tab", "container"), tabcont).addClass("active");
+            $("#" + me.attr("id").replace("tab", "container"), tab).addClass("active");
         }
     });
 };
 
-$(function() {
+$(function () {
     // init tabcontainers --------------------------------------------------------------------
     initTabContainer($('body'));
 });
