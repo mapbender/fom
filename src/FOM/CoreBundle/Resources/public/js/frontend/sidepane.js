8d7e745386f593112241339e046b11757321556f
Andriy Oblivantsev <eslider@gmail.com>
2014-12-03 17:01:43 +0100

    fix side pane animation

--- src/FOM/CoreBundle/Resources/public/js/frontend/sidepane.js
+++ src/FOM/CoreBundle/Resources/public/js/frontend/sidepane.js
@@ -1,7 +1,28 @@
 $(function() {
-    // init sidebar toggle -------------------------------------------------------------------
-    var sideBarToggle = function(){
-        $('#templateWrapper').toggleClass("sidePaneOpened");
-    };
-    $(".toggleSideBar").bind("click", sideBarToggle);
+
+    var switchButton = $(".toggleSideBar");
+    var sidePane = switchButton.closest("div.sidePane");
+    var templateWrapper = $('#templateWrapper');
+    var speed = 300;
+    var animation = {};
+
+    sidePane.data('isOpened', true);
+    switchButton.on('click', function() {
+        var isOpened = sidePane.data('isOpened');
+        var align = sidePane.hasClass('right') ? 'right' : 'left';
+
+        if(isOpened) {
+            animation[align] =  ["-" + sidePane.outerWidth(true) + "px", "swing"];
+            sidePane.animate(animation, speed, function() {
+                templateWrapper.removeClass("sidePaneOpened");
+                sidePane.data('isOpened', !isOpened);
+            });
+        } else {
+            templateWrapper.addClass("sidePaneOpened");
+            animation[align] = ["0px", "swing"];
+            sidePane.animate(animation, speed, function() {
+                sidePane.data('isOpened', !isOpened);
+            });
+        }
+    });
 });
