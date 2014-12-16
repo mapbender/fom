d222a4f7269975b218acdb41b6b2a31c742665f9
root <root@geo1.kv.o-p-r.de>
2014-12-16 15:15:11 +0100

    fix side pane animation

--- src/FOM/CoreBundle/Resources/public/js/frontend/sidepane.js
+++ src/FOM/CoreBundle/Resources/public/js/frontend/sidepane.js
@@ -7,19 +7,29 @@ $(function() {
     var animation = {};
 
     sidePane.data('isOpened', true);
+
+    sidePane.css({
+	'-webkit-transition': 'none !important',
+	'-moz-transition': 'none !important',
+	'-o-transition': 'none !important',
+	'-ms-transition': 'none !important',
+	'transition': 'none !important'
+    });
+
     switchButton.on('click', function() {
         var isOpened = sidePane.data('isOpened');
         var align = sidePane.hasClass('right') ? 'right' : 'left';
-
         if(isOpened) {
-            animation[align] =  ["-" + sidePane.outerWidth(true) + "px", "swing"];
-            sidePane.animate(animation, speed, function() {
-                templateWrapper.removeClass("sidePaneOpened");
+            animation[align] =  "-" + sidePane.outerWidth(true) + "px"; //, "swing"];
+            sidePane.animate(animation,speed, function() {
+                //templateWrapper.removeClass("sidePaneOpened");
                 sidePane.data('isOpened', !isOpened);
+		sidePane.find('.sideContent').css('display','none');
             });
         } else {
-            templateWrapper.addClass("sidePaneOpened");
-            animation[align] = ["0px", "swing"];
+            //templateWrapper.addClass("sidePaneOpened");
+            animation[align] = "0px";
+	    sidePane.find(".sideContent").css('display','block');
             sidePane.animate(animation, speed, function() {
                 sidePane.data('isOpened', !isOpened);
             });
