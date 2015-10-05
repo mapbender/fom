91df09a8294ec759296ca365c070e04a1435ee35
Paul Schmidt <paul.schmidt@wheregroup.com>
2015-10-05 15:47:40 +0200

    formate code, merge message

--- src/FOM/UserBundle/Controller/UserController.php
+++ src/FOM/UserBundle/Controller/UserController.php
@@ -36,10 +36,10 @@ class UserController extends Controller
     public function indexAction()
     {
         $securityContext = $this->get('security.context');
-        $oid = new ObjectIdentity('class', 'FOM\UserBundle\Entity\User');
+        $oid             = new ObjectIdentity('class', 'FOM\UserBundle\Entity\User');
 
-        $query = $this->getDoctrine()->getManager()->createQuery('SELECT r FROM FOMUserBundle:User r');
-        $users = $query->getResult();
+        $query         = $this->getDoctrine()->getManager()->createQuery('SELECT r FROM FOMUserBundle:User r');
+        $users         = $query->getResult();
         $allowed_users = array();
         // ACL access check
         foreach ($users as $index => $user) {
@@ -49,7 +49,7 @@ class UserController extends Controller
         }
 
         return array(
-            'users' => $allowed_users,
+            'users'             => $allowed_users,
             'create_permission' => $securityContext->isGranted('CREATE', $oid));
     }
 
@@ -61,9 +61,10 @@ class UserController extends Controller
     public function newAction()
     {
         $user = new User();
+
         // ACL access check
         $securityContext = $this->get('security.context');
-        $oid = new ObjectIdentity('class', get_class($user));
+        $oid             = new ObjectIdentity('class', get_class($user));
         if (false === $securityContext->isGranted('CREATE', $oid)) {
             throw new AccessDeniedException();
         }
@@ -73,19 +74,19 @@ class UserController extends Controller
             || $securityContext->isGranted('OWNER', $oid);
 
         $profile = $this->addProfileForm($user);
-        $form = $this->createForm(new UserType(), $user, array(
+        $form    = $this->createForm(new UserType(), $user, array(
             'profile_formtype' => $profile['formtype'],
             'group_permission' => $groupPermission,
-            'acl_permission' => $securityContext->isGranted('OWNER', $oid),
+            'acl_permission'   => $securityContext->isGranted('OWNER', $oid),
         ));
 
         return array(
-            'user' => $user,
-            'form' => $form->createView(),
-            'form_name' => $form->getName(),
-            'edit' => false,
+            'user'             => $user,
+            'form'             => $form->createView(),
+            'form_name'        => $form->getName(),
+            'edit'             => false,
             'profile_template' => $profile['template'],
-            'profile_assets' => $profile['assets']);
+            'profile_assets'   => $profile['assets']);
     }
 
     /**
@@ -99,7 +100,7 @@ class UserController extends Controller
 
         // ACL access check
         $securityContext = $this->get('security.context');
-        $oid = new ObjectIdentity('class', get_class($user));
+        $oid             = new ObjectIdentity('class', get_class($user));
         if (false === $securityContext->isGranted('CREATE', $oid)) {
             throw new AccessDeniedException();
         }
@@ -108,12 +109,11 @@ class UserController extends Controller
             $securityContext->isGranted('EDIT', new ObjectIdentity('class', 'FOM\UserBundle\Entity\Group'))
             || $securityContext->isGranted('OWNER', $oid);
 
-
         $profile = $this->addProfileForm($user);
-        $form = $this->createForm(new UserType(), $user, array(
+        $form    = $this->createForm(new UserType(), $user, array(
             'profile_formtype' => $profile['formtype'],
             'group_permission' => $groupPermission,
-            'acl_permission' => $securityContext->isGranted('OWNER', $oid),
+            'acl_permission'   => $securityContext->isGranted('OWNER', $oid),
         ));
 
         $form->bind($this->get('request'));
@@ -138,12 +138,15 @@ class UserController extends Controller
 
                 // SQLite needs a flush here
                 $em->flush();
+
                 // Check and persists profile if exists
                 if ($profile) {
                     $profile->setUid($user);
                     $em->persist($profile);
                 }
+
                 $em->flush();
+
                 $em->getConnection()->commit();
 
                 if ($form->has('acl')) {
@@ -167,15 +170,15 @@ class UserController extends Controller
             return $this->redirect($this->generateUrl('fom_user_user_index'));
         }
 
-        $this->get('session')->getFlashBag()->set('error', 'Your application has been saved.');
+        $this->get('session')->getFlashBag()->set('error', 'There field validation errors.');
 
         return array(
-            'user' => $user,
-            'form' => $form->createView(),
-            'form_name' => $form->getName(),
-            'edit' => false,
+            'user'             => $user,
+            'form'             => $form->createView(),
+            'form_name'        => $form->getName(),
+            'edit'             => false,
             'profile_template' => $profile['template'],
-            'profile_assets' => $profile['assets']);
+            'profile_assets'   => $profile['assets']);
     }
 
     /**
@@ -201,21 +204,20 @@ class UserController extends Controller
             || $securityContext->isGranted('OWNER', $user);
 
         $profile = $this->addProfileForm($user);
-        $form = $this->createForm(new UserType(), $user, array(
-            'requirePassword' => false,
+        $form    = $this->createForm(new UserType(), $user, array(
+            'requirePassword'  => false,
             'profile_formtype' => $profile['formtype'],
             'group_permission' => $groupPermission,
-            'acl_permission' => $securityContext->isGranted('OWNER', $user)
+            'acl_permission'   => $securityContext->isGranted('OWNER', $user)
         ));
 
-
         return array(
-            'user' => $user,
-            'form' => $form->createView(),
-            'form_name' => $form->getName(),
-            'edit' => true,
+            'user'             => $user,
+            'form'             => $form->createView(),
+            'form_name'        => $form->getName(),
+            'edit'             => true,
             'profile_template' => $profile['template'],
-            'profile_assets' => $profile['assets']);
+            'profile_assets'   => $profile['assets']);
     }
 
     /**
@@ -236,12 +238,12 @@ class UserController extends Controller
             throw new AccessDeniedException();
         }
         // If no password is given, we'll recycle the old one
-        $request = $this->get('request');
-        $userData = $request->get('user');
+        $request      = $this->get('request');
+        $userData     = $request->get('user');
         $keepPassword = false;
         if ($userData['password']['first'] === '' && $userData['password']['second'] === '') {
             $userData['password'] = array(
-                'first' => $user->getPassword(),
+                'first'  => $user->getPassword(),
                 'second' => $user->getPassword());
 
             $keepPassword = true;
@@ -255,11 +257,11 @@ class UserController extends Controller
             || $securityContext->isGranted('OWNER', $user);
 
         $profile = $this->addProfileForm($user);
-        $form = $this->createForm(new UserType(), $user, array(
-            'requirePassword' => false,
+        $form    = $this->createForm(new UserType(), $user, array(
+            'requirePassword'  => false,
             'profile_formtype' => $profile['formtype'],
             'group_permission' => $groupPermission,
-            'acl_permission' => $securityContext->isGranted('OWNER', $user)
+            'acl_permission'   => $securityContext->isGranted('OWNER', $user)
         ));
         $form->bind($userData);
 
@@ -289,12 +291,12 @@ class UserController extends Controller
         }
 
         return array(
-            'user' => $user,
-            'form' => $form->createView(),
-            'form_name' => $form->getName(),
-            'edit' => true,
+            'user'             => $user,
+            'form'             => $form->createView(),
+            'form_name'        => $form->getName(),
+            'edit'             => true,
             'profile_template' => $profile['template'],
-            'profile_assets' => $profile['assets']);
+            'profile_assets'   => $profile['assets']);
     }
 
     /**
@@ -306,20 +308,22 @@ class UserController extends Controller
     public function deleteAction($id)
     {
         $user = $this->getDoctrine()->getRepository('FOMUserBundle:User')->find($id);
+
         if ($user === null) {
             throw new NotFoundHttpException('The user does not exist');
         }
         if ($user->getId() === 1) {
             throw new NotFoundHttpException('The root user can not be deleted');
         }
+
         // ACL access check
         $securityContext = $this->get('security.context');
         if (false === $securityContext->isGranted('DELETE', $user)) {
             throw new AccessDeniedException();
         }
 
-	$aclProvider = $this->get('security.acl.provider');
-        $oid = ObjectIdentity::fromDomainObject($user);
+        $aclProvider = $this->get('security.acl.provider');
+        $oid         = ObjectIdentity::fromDomainObject($user);
         $aclProvider->deleteAcl($oid);
 
         $this->addProfileForm($user);
@@ -331,7 +335,7 @@ class UserController extends Controller
             $em->getConnection()->beginTransaction();
 
             $aclProvider = $this->get('security.acl.provider');
-            $oid = ObjectIdentity::fromDomainObject($user);
+            $oid         = ObjectIdentity::fromDomainObject($user);
             $aclProvider->deleteAcl($oid);
 
             $em->remove($user);
@@ -344,23 +348,23 @@ class UserController extends Controller
             $this->get('session')->getFlashBag()->set('success', 'The user has been deleted.');
         } catch (Exception $e) {
             $em->getConnection()->rollback();
-            $this->get('session')->getFlashBag()->set('error', 'The user couldn\'t be deleted.');
+            $this->get('session')->getFlashBag()->set('error', "The user couldn't be deleted.");
         }
         return new Response();
     }
 
     private function addProfileForm(User $user)
     {
-        $container = $this->container;
-        $profileEntity = $container->getParameter('fom_user.profile_entity');
+        $container       = $this->container;
+        $profileEntity   = $container->getParameter('fom_user.profile_entity');
         $profileFormType = $container->getParameter('fom_user.profile_formtype');
         $profileTemplate = $container->getParameter('fom_user.profile_template');
-        $profileAssets = $container->getParameter('fom_user.profile_assets');
+        $profileAssets   = $container->getParameter('fom_user.profile_assets');
 
         return array(
             'formtype' => $profileFormType,
             'template' => $profileTemplate,
-            'assets' => $profileAssets
+            'assets'   => $profileAssets
         );
     }
 }
