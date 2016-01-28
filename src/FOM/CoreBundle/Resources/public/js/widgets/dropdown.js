564297cd95e69514379b758d9127c9678f82c0a2
Andriy Oblivantsev <eslider@gmail.com>
2016-01-28 21:02:38 +0100

    Fix select element

--- src/FOM/CoreBundle/Resources/public/js/widgets/dropdown.js
+++ src/FOM/CoreBundle/Resources/public/js/widgets/dropdown.js
@@ -1,26 +1,28 @@
-$(function() {
-    $('.dropdown').each(function() {
-        var me = $(this);
-        var dropdownList = me.find(".dropdownList");
-        if(dropdownList.children().length === 0) {
-            me.find("option").each(function(i, e) {
-                $(e).addClass("opt-" + i);
-                dropdownList.append($('<li class="item-' + i + '">' + $(e).text() + '</li>'));
-            });
-        }
-        var select = me.find("select").val();
-        me.find(".dropdownValue").text(me.find('option[value="' + select + '"]').text())
-    });
-    $(document).on("click", ".dropdown", function() {
+var initDropdown = function () {
+    var me = $(this);
+    var dropdownList = me.find(".dropdownList");
+    if (dropdownList.children().length === 0) {
+        me.find("option").each(function (i, e) {
+            $(e).addClass("opt-" + i);
+            dropdownList.append($('<li class="item-' + i + '">' + $(e).text() + '</li>'));
+        });
+    }
+    var select = me.find("select").val();
+    me.find(".dropdownValue").text(me.find('option[value="'+select+'"]').text())
+};
+$(function () {
+    // init dropdown list --------------------------------------------------------------------
+
+    var toggleList = function () {
         var me = $(this);
         var list = me.find(".dropdownList");
         var opts = me.find(".hiddenDropdown");
-        if(list.css("display") == "block") {
+        if (list.css("display") == "block") {
             list.hide();
         } else {
             $(".dropdownList").hide();
             list.show();
-            list.find("li").one("click", function(event) {
+            list.find("li").one("click", function (event) {
                 event.stopPropagation();
                 list.hide().find("li").off("click");
                 var me2 = $(this);
@@ -32,9 +34,13 @@ $(function() {
             })
         }
 
-        $(document).one("click", function() {
+        $(document).one("click", function () {
             list.hide().find("li").off("mouseout").off("click");
         });
         return false;
+    }
+    $('.dropdown').each(function () {
+        initDropdown.call(this);
     });
+    $(document).on("click", ".dropdown", toggleList);
 });
