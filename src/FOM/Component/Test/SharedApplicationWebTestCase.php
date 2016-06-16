92f253fcc2b503aafd62e0968142cf03e71ff16b
Andriy Oblivantsev <eslider@gmail.com>
2016-06-16 11:24:21 +0200

    Deprecate FOM SharedApplicationWebTestCase

--- src/FOM/Component/Test/SharedApplicationWebTestCase.php
+++ src/FOM/Component/Test/SharedApplicationWebTestCase.php
@@ -2,42 +2,13 @@
 
 namespace FOM\Component\Test;
 
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Input\StringInput;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Mapbender\CoreBundle\Tests\TestBase;
 
-class SharedApplicationWebTestCase extends WebTestCase
+/**
+ * Class SharedApplicationWebTestCase
+ *
+ * @deprecated will be removed in 3.0.6
+ */
+class SharedApplicationWebTestCase extends TestBase
 {
-    protected static $client;
-    protected static $application;
-    protected static $options = array();
-
-    public static function setUpBeforeClass()
-    {
-        //self::runCommand('cache:clear --no-debug');
-        //self::runCommand('cache:warmup --no-debug');
-        self::runCommand('doctrine:database:drop --force');
-        self::runCommand('doctrine:database:create');
-        self::runCommand('doctrine:schema:create');
-        self::runCommand('fom:user:resetroot --username=root --password=root --email=root@example.com --silent');
-        self::runCommand('doctrine:fixtures:load --fixtures=./mapbender/src/Mapbender/CoreBundle/DataFixtures/ORM/Epsg/ --append');
-    }
-
-    protected static function runCommand($command) {
-        $command = sprintf('%s --quiet', $command);
-        return self::getApplication()->run(new StringInput($command));
-    }
-
-
-    protected static function getApplication() {
-        if(!static::$application) {
-            $options = static::$options;
-            static::$client = static::createClient($options);
-
-            static::$application = new Application(static::$client->getKernel());
-            static::$application->setAutoExit(false);
-        }
-
-        return static::$application;
-    }
 }
