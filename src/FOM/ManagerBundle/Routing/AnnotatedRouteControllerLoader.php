4ac4c460d66ba121361d4a7d00ea5c19f935d128
Andriy Oblivantsev <eslider@gmail.com>
2016-09-20 07:22:55 +0200

    Fix deprecated call by AnnotatedRouteControllerLoader route  configuring

--- src/FOM/ManagerBundle/Routing/AnnotatedRouteControllerLoader.php
+++ src/FOM/ManagerBundle/Routing/AnnotatedRouteControllerLoader.php
@@ -10,7 +10,9 @@ use Doctrine\Common\Annotations\Reader;
 /**
  * Route annotation loader
  *
- * This loader extends the one in the FrameworkExtraBundle by prefixing manager routes with a configurable prefix.
+ * This loader extends the one in the
+ * FrameworkExtraBundle by prefixing manager
+ * routes with a configurable prefix.
  *
  * @author Christian Wygoda
  */
@@ -25,15 +27,17 @@ class AnnotatedRouteControllerLoader extends BaseLoader
     }
 
     /**
-     * For all route annotations using FOM\ManagerBundle\Configuration\Route, this adds the configured prefix.
+     * For all route annotations using
+     * FOM\ManagerBundle\Configuration\Route,
+     * this adds the configured prefix.
      *
      * @inheritdoc
      */
     protected function configureRoute(Route $route, \ReflectionClass $class, \ReflectionMethod $method, $annot)
     {
         parent::configureRoute($route, $class, $method, $annot);
-        if(is_a($annot, 'FOM\ManagerBundle\Configuration\Route')) {
-            $route->setPattern($this->prefix . $route->getPattern());
+        if (is_a($annot, 'FOM\ManagerBundle\Configuration\Route')) {
+            $route->setPath($this->prefix . $route->getPath());
         }
     }
 }
