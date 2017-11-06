f3206dd8845a248e77f9f07218badde0980433ba
David Patzke <david.patzke@wheregroup.com>
2017-11-06 14:51:16 +0100

    Extend getAllUser and getAllGroups with LDAPSupport

--- src/FOM/UserBundle/Component/FOMIdentitiesProvider.php
+++ src/FOM/UserBundle/Component/FOMIdentitiesProvider.php
@@ -2,7 +2,9 @@
 
 namespace FOM\UserBundle\Component;
 
+use FOM\UserBundle\Entity\Group;
 use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\Ldap\LdapClientInterface;
 
 /**
  * Managed users
@@ -15,21 +17,13 @@ class FOMIdentitiesProvider extends ContainerAware implements IdentitiesProvider
 {
 
     /**
-     * @return \Doctrine\Bundle\DoctrineBundle\Registry
-     */
-    protected function getDoctrine()
-    {
-        return $this->container->get('doctrine');
-    }
-
-    /**
      * @param string $search
      * @return array
      */
     public function getUsers($search)
     {
         $repo = $this->getDoctrine()->getRepository('FOMUserBundle:User');
-        $qb = $repo->createQueryBuilder('u');
+        $qb   = $repo->createQueryBuilder('u');
 
         $query = $qb->where($qb->expr()->like('LOWER(u.username)', ':search'))
             ->setParameter(':search', '%' . strtolower($search) . '%')
@@ -37,80 +31,109 @@ class FOMIdentitiesProvider extends ContainerAware implements IdentitiesProvider
             ->getQuery();
 
         $result = array();
-        foreach($query->getResult() as $user) {
+        foreach ($query->getResult() as $user) {
             $result[] = 'u:' . $user->getUsername();
         }
         return $result;
     }
 
-    public function getRoles() {
-        $repo = $this->getDoctrine()->getRepository('FOMUserBundle:Group');
+    /**
+     * @return \Doctrine\Bundle\DoctrineBundle\Registry
+     */
+    protected function getDoctrine()
+    {
+        return $this->container->get('doctrine');
+    }
+
+    public function getRoles()
+    {
+        $repo   = $this->getDoctrine()->getRepository('FOMUserBundle:Group');
         $groups = $repo->findAll();
 
         $roles = array();
-        foreach($groups as $group) {
+        foreach ($groups as $group) {
             $roles[] = 'r:' . $group->getAsRole();
         }
 
         return $roles;
     }
 
-    public function getAllGroups(){
-        $repo = $this->getDoctrine()->getRepository('FOMUserBundle:Group');
-        $groups = $repo->findAll();
-
+    public function getAllGroups()
+    {
         $all = array();
-        foreach($groups as $group) {
-            $all[] = $group;
+        if ($this->container->hasParameter('ldap_host')) {
+            $groupDn        = $this->container->getParameter('ldap_group_dn');
+            $ldapClient   = $this->getLdapClient();
+            $ldapGroupList = $ldapClient->find($groupDn, '(objectClass=top)');
+            if ($ldapGroupList != null) {
+                foreach (array_slice($ldapGroupList, 2) as $ldapGroup) {
+                    $group =    new Group();
+                    $group->setTitle( $ldapGroup['cn'][0]);
+                    $all[] = $group;
+                }
+
+            }
+
         }
 
+
+        $repo   = $this->getDoctrine()->getRepository('FOMUserBundle:Group');
+        $groups = $repo->findAll();
+
+        foreach ($groups as $group) {
+
+            $all[] = $group;
+        }
         return $all;
     }
 
-    public function getAllUsers(){
+    public function getAllUsers()
+    {
         // Settings for LDAP
-        if($this->container->hasParameter('ldap_host')) {
-            $ldapHostname = $this->container->getParameter("ldap_host");
-            $ldapPort = $this->container->getParameter("ldap_port");
-            $ldapVersion = $this->container->getParameter("ldap_version");
-            $baseDn = $this->container->getParameter("ldap_user_base_dn");
-            $nameAttribute = $this->container->getParameter("ldap_user_name_attribute");
-            $bindDn = $this->container->getParameter("ldap_bind_dn");
-            $bindPasswd = $this->container->getParameter("ldap_bind_pwd");
-            $filter = "(" . $nameAttribute . "=*)";
+        $all = array();
+        if ($this->container->hasParameter('ldap_host')) {
+            $nameAttribute = $this->container->getParameter('ldap_group_name_attribute');
+            $userDn        = $this->container->getParameter('ldap_user_base_dn');
 
-            $connection = @ldap_connect($ldapHostname, $ldapPort);
-            ldap_set_option($connection, LDAP_OPT_PROTOCOL_VERSION, $ldapVersion);
+            $ldapClient   = $this->getLdapClient();
+            $ldapUserList = $ldapClient->find($userDn, '(objectclass=top)');
+            if ($ldapUserList !=  null) {
+                unset($ldapUserList[0]);
+                foreach (array_slice($ldapUserList, 1) as $ldapUser) {
 
-            if (strlen($bindDn) !== 0 && strlen($bindPasswd) !== 0) {
-                if (!ldap_bind($connection, $bindDn, $bindPasswd)) {
-                    throw new \Exception('Unable to bind LDAP to DN: ' . $bindDn);
+                    $userName = $ldapUser[ $nameAttribute ][0];
+                    $user     = new \stdClass();
+                    $user->getUsername = $ldapUser[ $nameAttribute ][0];
+                    $all[] = $user;
                 }
+
             }
 
-            $ldapListRequest = ldap_list($connection, $baseDn, $filter); // or throw exeption('Unable to list. LdapError: ' . ldap_error($ldapConnection));
-
-            $ldapUserList = ldap_get_entries($connection, $ldapListRequest);
         }
 
         // Settings for local user database
-        $repo = $this->getDoctrine()->getRepository('FOMUserBundle:User');
+        $repo  = $this->getDoctrine()->getRepository('FOMUserBundle:User');
         $users = $repo->findAll();
 
-        $all = array();
-
-        if($this->container->hasParameter('ldap_host')) {
-            // Add Users from LDAP
-            foreach($ldapUserList as $ldapUser) {
-                $user = new \stdClass;
-                $user->getUsername = $ldapUser[$nameAttribute][0];
-                $all[] = $user;
-            }
-        }
         // Add Mapbenderusers from database
-        foreach($users as $user) {
+        foreach ($users as $user) {
             $all[] = $user;
         }
         return $all;
+    }
+
+    /**
+     * @return LdapClientInterface
+     */
+    private function getLdapClient()
+    {
+
+
+        $ldapClient = $this->container->get('ldapClient');
+        $bindDn     = $this->container->getParameter("ldap_bind_dn");
+        $bindPasswd = $this->container->getParameter("ldap_bind_pwd");
+        $ldapClient->bind($bindDn, $bindPasswd);
+
+        return $ldapClient;
     }
 }
