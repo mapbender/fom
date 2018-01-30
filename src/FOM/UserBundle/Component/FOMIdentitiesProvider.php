68dea5583ec8a5f32b7bbf19daaffd8c2c7440cd
David Patzke <davidpatzke@gmx.de>
2018-01-30 11:32:51 +0100

    Refactoring
    * Add type declaration for some vars
    * Change ContainerAware with ContainerAwareTrait
    * Add some phpdocs(WIP)

--- src/FOM/UserBundle/Component/FOMIdentitiesProvider.php
+++ src/FOM/UserBundle/Component/FOMIdentitiesProvider.php
@@ -2,8 +2,11 @@
 
 namespace FOM\UserBundle\Component;
 
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\ORM\EntityRepository;
 use FOM\UserBundle\Entity\Group;
-use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 use Symfony\Component\Ldap\LdapClientInterface;
 
 /**
@@ -13,8 +16,9 @@ use Symfony\Component\Ldap\LdapClientInterface;
  * @alias User Manager
  *d
  */
-class FOMIdentitiesProvider extends ContainerAware implements IdentitiesProviderInterface
+class FOMIdentitiesProvider implements IdentitiesProviderInterface, ContainerAwareInterface
 {
+    use ContainerAwareTrait;
 
     /**
      * @param string $search
@@ -22,7 +26,9 @@ class FOMIdentitiesProvider extends ContainerAware implements IdentitiesProvider
      */
     public function getUsers($search)
     {
-        $repo = $this->getDoctrine()->getRepository('FOMUserBundle:User');
+
+
+        $repo = $this->getUserRepository();
         $qb   = $repo->createQueryBuilder('u');
 
         $query = $qb->where($qb->expr()->like('LOWER(u.username)', ':search'))
@@ -30,27 +36,49 @@ class FOMIdentitiesProvider extends ContainerAware implements IdentitiesProvider
             ->orderBy('u.username', 'ASC')
             ->getQuery();
 
+        /* @var array $users */
+        $users = $query->getResult();
         $result = array();
-        foreach ($query->getResult() as $user) {
+        foreach ($users as $user) {
             $result[] = 'u:' . $user->getUsername();
         }
         return $result;
     }
 
     /**
-     * @return \Doctrine\Bundle\DoctrineBundle\Registry
+     * @return EntityRepository
+     */
+    protected function getUserRepository()
+    {
+
+
+        return $this->getDoctrine()->getRepository('FOMUserBundle:User');
+
+    }
+
+    /**
+     * @return Registry
      */
     protected function getDoctrine()
     {
-        return $this->container->get('doctrine');
+        /* @var Registry  $doctrine*/
+        $doctrine =  $this->container->get('doctrine');
+        return $doctrine;
     }
 
+    /**
+     * @return array
+     */
     public function getRoles()
     {
         $repo   = $this->getDoctrine()->getRepository('FOMUserBundle:Group');
+        /* @var array $groups */
         $groups = $repo->findAll();
 
+
         $roles = array();
+
+        /* @var Group $group */
         foreach ($groups as $group) {
             $roles[] = 'r:' . $group->getAsRole();
         }
@@ -65,21 +93,20 @@ class FOMIdentitiesProvider extends ContainerAware implements IdentitiesProvider
     {
         $all = array();
         if ($this->container->hasParameter('ldap.host')) {
-            $groupDn        = $this->container->getParameter('ldap.group.baseDn');
-            $groupFilter        = $this->container->getParameter('ldap.group.adminFilter');
-            $ldapClient   = $this->getLdapClient();
+            $groupDn       = $this->container->getParameter('ldap.group.baseDn');
+            $groupFilter   = $this->container->getParameter('ldap.group.adminFilter');
+            $ldapClient    = $this->getLdapClient();
             $ldapGroupList = $ldapClient->find($groupDn, $groupFilter);
             if ($ldapGroupList != null) {
                 foreach (array_slice($ldapGroupList, 2) as $ldapGroup) {
-                    $group =    new Group();
-                    $group->setTitle( $ldapGroup['cn'][0]);
+                    $group = new Group();
+                    $group->setTitle($ldapGroup['cn'][0]);
                     $all[] = $group;
                 }
 
             }
 
         }
-
 
         $repo   = $this->getDoctrine()->getRepository('FOMUserBundle:Group');
         $groups = $repo->findAll();
@@ -92,26 +119,42 @@ class FOMIdentitiesProvider extends ContainerAware implements IdentitiesProvider
     }
 
     /**
+     * @return LdapClientInterface
+     */
+    private function getLdapClient()
+    {
+
+        /** @var LdapClientInterface $ldapClient */
+        $ldapClient = $this->container->get('ldapClient');
+        $bindDn     = $this->container->getParameter("ldap.bind.dn");
+
+        $bindPwd = $this->container->getParameter("ldap.bind.pwd");
+        $ldapClient->bind($bindDn, $bindPwd);
+
+        return $ldapClient;
+    }
+
+    /**
      * @return array
      */
-    public  function getAllUsers()
+    public function getAllUsers()
     {
         // Settings for LDAP
         $all = array();
         if ($this->container->hasParameter('ldap.host')) {
             $nameAttribute = $this->container->getParameter('ldap.user.nameAttribute');
             $userDn        = $this->container->getParameter('ldap.user.baseDn');
-            $userQuery        = $this->container->getParameter('ldap.user.adminFilter');
-
+            $userQuery     = $this->container->getParameter('ldap.user.adminFilter');
 
             $ldapClient   = $this->getLdapClient();
             $ldapUserList = $ldapClient->find($userDn, $userQuery);
 
-            if ($ldapUserList !=  null) {
-                foreach (array_slice($ldapUserList, 2) as $ldapUser) {
-                    $user     = new \stdClass();
+            if ($ldapUserList != null) {
+                unset($ldapUserList[0]);
+                foreach (array_slice($ldapUserList, 1) as $ldapUser) {
+                    $user              = new \stdClass();
                     $user->getUsername = $ldapUser[ $nameAttribute ][0];
-                    $all[] = $user;
+                    $all[]             = $user;
                 }
 
             }
@@ -127,21 +170,5 @@ class FOMIdentitiesProvider extends ContainerAware implements IdentitiesProvider
             $all[] = $user;
         }
         return $all;
-    }
-
-    /**
-     * @return LdapClientInterface
-     */
-    private function getLdapClient()
-    {
-
-
-        $ldapClient = $this->container->get('ldapClient');
-        $bindDn     = $this->container->getParameter("ldap.bind.dn");
-
-        $bindPwd = $this->container->getParameter("ldap.bind.pwd");
-        $ldapClient->bind($bindDn, $bindPwd);
-
-        return $ldapClient;
     }
 }
