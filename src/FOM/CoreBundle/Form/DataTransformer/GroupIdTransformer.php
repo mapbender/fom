c330102fbb047533d30f307f037fa205e8e8db36
wg-rolf <rolf.neuberger@wheregroup.com>
2018-02-20 16:51:05 +0100

    [NFC] fix type annotations and extreme style violations

--- src/FOM/CoreBundle/Form/DataTransformer/GroupIdTransformer.php
+++ src/FOM/CoreBundle/Form/DataTransformer/GroupIdTransformer.php
@@ -30,38 +30,32 @@ class GroupIdTransformer implements DataTransformerInterface
     /**
      * Transforms an object (group) to a string (id).
      *
-     * @param  Group|null $id
-     * @return string
+     * @param  string|string[]|null $id group ids
+     * @return Group|ArrayCollection|Group[]|null
      */
     public function transform($id)
     {
-        if(null === $id)
-        {
+        if (null === $id) {
             return null;
-        }
-        else if(is_string($id))
-        {
-            return $this->om->getRepository('FOMUserBundle:Group')
-                    ->findOneBy(array('id' => $id));
-        }
-        else if(is_array($id))
-        {
-            if(count($id) === 0)
+        } else if (is_string($id)) {
+            /** @var Group|null $entity */
+            $entity = $this->om->getRepository('FOMUserBundle:Group')
+                ->findOneBy(array('id' => $id));
+            return $entity;
+        } elseif (is_array($id)) {
+            if (count($id) === 0) {
                 return null;
+            }
             $result = new ArrayCollection();
-            foreach($id as $value)
-            {
+            foreach($id as $value) {
                 $group = $this->om->getRepository('FOMUserBundle:Group')
                     ->findOneBy(array('id' => $value));
-                if($group !== null)
-                {
+                if ($group !== null) {
                     $result->add($group);
                 }
             }
             return $result;
-        }
-        else
-        {
+        } else {
             return null;
         }
     }
@@ -69,20 +63,20 @@ class GroupIdTransformer implements DataTransformerInterface
     /**
      * Transforms a string (id) to an object (group).
      *
-     * @param  string $id
-     * @return Group|null
+     * @param  Group|ArrayCollection|Group[]|null $group
+     * @return string|string[]
      * @throws TransformationFailedException if object (group) is not found.
      */
     public function reverseTransform($group)
     {
-        if(null === $group) return "";
-        else if($group instanceof Group) return (string) $group->getId();
-        else if($group instanceof ArrayCollection)
-        {
+        if(null === $group) {
+            return "";
+        } elseif ($group instanceof Group) {
+            return (string)$group->getId();
+        } elseif ($group instanceof ArrayCollection) {
             $result = array();
-            foreach($group as $value)
-            {
-                $result[] = $value->getId();
+            foreach($group as $value) {
+                $result[] = (string)$value->getId();
             }
             return $result;
         }
--- src/FOM/UserBundle/Component/RolesService.php
+++ src/FOM/UserBundle/Component/RolesService.php
@@ -3,13 +3,16 @@
 namespace FOM\UserBundle\Component;
 
 use FOM\ManagerBundle\Component\ManagerBundle;
+use Symfony\Component\HttpKernel\Kernel;
 
 class RolesService
 {
+    /** @var Kernel */
     protected $kernel;
+    /** @var string[] */
     protected $roles;
 
-    public function __construct(\AppKernel $kernel)
+    public function __construct(Kernel $kernel)
     {
         $this->kernel = $kernel;
     }
@@ -18,13 +21,16 @@ class RolesService
     {
         $this->roles = array();
         foreach($this->kernel->getBundles() as $bundle) {
-            if(is_subclass_of($bundle, 'FOM\ManagerBundle\Component\ManagerBundle')) {
+            if ($bundle instanceof ManagerBundle) {
                 $bundle_roles = $bundle->getRoles();
                 $this->roles = array_merge($this->roles, $bundle_roles);
             }
         }
     }
 
+    /**
+     * @return string[]
+     */
     public function getAll()
     {
         if($this->roles === null) {
