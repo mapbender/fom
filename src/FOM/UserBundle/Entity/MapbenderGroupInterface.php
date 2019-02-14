953fd7b6838cdf0c130a3bc3c7848902e7a311bc
David Patzke <david.patzke@wheregroup.com>
2019-02-14 08:24:45 +0100

    Add interface for MapbenderGroups to define mandatory propoerties in the future

---
+++ src/FOM/UserBundle/Entity/MapbenderGroupInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FOM\UserBundle\Entity;
+
+
+interface MapbenderGroupInterface {
+
+    public function setTitle($title);
+    public function getTitle();
+    public function getAsRole();
+
+
+}
