b57b8be745e588bba1494c8eaed32758152a5de5
wg-rolf <rolf.neuberger@wheregroup.com>
2019-05-09 12:28:06 +0200

    Remove LoginController::loginAction (migrating to Mapbender)

--- src/FOM/UserBundle/Controller/LoginController.php
+++ src/FOM/UserBundle/Controller/LoginController.php
@@ -3,9 +3,6 @@ namespace FOM\UserBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\Security;
 
 /**
  * User controller.
@@ -15,40 +12,6 @@ use Symfony\Component\Security\Core\Security;
  */
 class LoginController extends Controller
 {
-    /**
-     * User login
-     *
-     * @Route("/user/login", methods={"GET"})
-     * @param Request $request
-     * @return Response
-     */
-    public function loginAction(Request $request)
-    {
-        $session = $request->getSession();
-
-        // get the login error if there is one
-        if ($request->attributes->has(Security::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(
-                Security::AUTHENTICATION_ERROR
-            );
-        } elseif (null !== $session && $session->has(Security::AUTHENTICATION_ERROR)) {
-            $error = $session->get(Security::AUTHENTICATION_ERROR);
-            $session->remove(Security::AUTHENTICATION_ERROR);
-        } else {
-            $error = '';
-        }
-
-        // last username entered by the user
-        $lastUsername = (null === $session) ? '' : $session->get(Security::LAST_USERNAME);
-
-        return $this->render('@FOMUser/Login/login.html.twig', array(
-            'last_username' => $lastUsername,
-            'error' => $error,
-            'selfregister' => $this->container->getParameter("fom_user.selfregister"),
-            'reset_password' => $this->container->getParameter("fom_user.reset_password"),
-        ));
-    }
-
     /**
      * @Route("/user/login/check")
      */
