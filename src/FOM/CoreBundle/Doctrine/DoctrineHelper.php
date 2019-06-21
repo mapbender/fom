7152c4f73ba4cdea7a0c7d0c9127d1ed654fea80
werrolf <rolf.neuberger@wheregroup.com>
2019-06-21 12:03:17 +0200

    Revert "Improve DoctrineHelper to get create tables for new entities if connection is sqlite"
    This reverts commit 17a666d5dad9d549f381b3adf8c5294f91732742.

--- src/FOM/CoreBundle/Doctrine/DoctrineHelper.php
+++ src/FOM/CoreBundle/Doctrine/DoctrineHelper.php
@@ -3,7 +3,6 @@ namespace FOM\CoreBundle\Doctrine;
 
 use Doctrine\Bundle\DoctrineBundle\Registry;
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Schema\SqliteSchemaManager;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\Tools\SchemaTool;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -31,52 +30,8 @@ class DoctrineHelper
         $connection    = $doctrine->getConnection();
         $schemaManager = $connection->getSchemaManager();
         $classMetadata = $manager->getClassMetadata($className);
-
         if ($force || !$schemaManager->tablesExist($classMetadata->getTableName())) {
-
-            if ($schemaManager instanceof SqliteSchemaManager) {
-
-                $columns     = array();
-                $identifiers = $classMetadata->getIdentifierColumnNames();
-                foreach ($classMetadata->getFieldNames() as $fieldName) {
-                    $fieldMapping = $classMetadata->getFieldMapping($fieldName);
-                    $columnSql    = $fieldMapping["fieldName"];
-                    switch ($fieldMapping["type"]) {
-                        case 'integer':
-                            $columnSql .= " INTEGER ";
-                            break;
-
-                        case 'real':
-                        case 'double':
-                        case 'float':
-                            $columnSql .= " REAL ";
-                            break;
-
-                        case 'datetime':
-                        case 'date':
-                        case 'boolean':
-                            $columnSql .= " INTEGER ";
-                            break;
-
-                        case 'blob':
-                        case 'file':
-                            $columnSql .= " BLOB ";
-                            break;
-
-                        default:
-                            $columnSql .= " TEXT ";
-                    }
-
-                    // PRIMARY KEY
-                    in_array($fieldName, $identifiers) && $columnSql .= "PRIMARY KEY ";
-                    $columnSql .= $fieldMapping["nullable"] ? "NULL " : "NOT NULL ";
-                    $columns[] = $columnSql;
-                }
-                $sql       = 'CREATE TABLE IF NOT EXISTS ' . $classMetadata->getTableName() . '( ' . implode(",\n", $columns) . ')';
-                $statement = $connection->query($sql);
-            } else {
-                $schemaTool->updateSchema(array($classMetadata), true);
-            }
+            $schemaTool->updateSchema(array($classMetadata), true);
         }
     }
 }
