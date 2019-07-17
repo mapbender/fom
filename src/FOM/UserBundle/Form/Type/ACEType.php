0eae7091701e70c2372bbdc7ad7369945c3e0d56
werrolf <rolf.neuberger@wheregroup.com>
2019-07-17 10:53:12 +0200

    Turn ACEModelDataTransformer into DI-pluggable service form.ace.model_transformer

--- src/FOM/UserBundle/Form/Type/ACEType.php
+++ src/FOM/UserBundle/Form/Type/ACEType.php
@@ -1,31 +1,23 @@
 <?php
 namespace FOM\UserBundle\Form\Type;
 
-use FOM\UserBundle\Component\Ldap;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\DataTransformerInterface;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Security\Acl\Model\AclProviderInterface;
 use FOM\ManagerBundle\Form\Type\TagboxType;
-
-use FOM\UserBundle\Form\DataTransformer\ACEDataTransformer;
 
 class ACEType extends AbstractType
 {
-    /** @var AclProviderInterface  */
-    protected $aclProvider;
-
-    protected $ldapUserProvider;
+    /** @var DataTransformerInterface */
+    protected $modelTransformer;
 
     /**
-     * ACEType constructor.
-     * @param AclProviderInterface $aclProvider
-     * @param Ldap\UserProvider $ldapUserProvider
+     * @param DataTransformerInterface $modelTransformer
      */
-    public function __construct(AclProviderInterface $aclProvider, Ldap\UserProvider $ldapUserProvider)
+    public function __construct(DataTransformerInterface $modelTransformer)
     {
-        $this->aclProvider = $aclProvider;
-        $this->ldapUserProvider = $ldapUserProvider;
+        $this->modelTransformer = $modelTransformer;
     }
 
     public function getName()
@@ -35,8 +27,7 @@ class ACEType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $transformer = new ACEDataTransformer($this->ldapUserProvider);
-        $builder->addModelTransformer($transformer);
+        $builder->addModelTransformer($this->modelTransformer);
 
         $builder->add('sid', 'text', array(
             'required' => true,
