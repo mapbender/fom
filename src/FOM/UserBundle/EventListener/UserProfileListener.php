e8e96a68610726d9ebc20ae802e342b853ab8a69
werrolf <rolf.neuberger@wheregroup.com>
2020-01-30 10:09:26 +0100

    Update UserProfileListener to respect user entity class

--- src/FOM/UserBundle/EventListener/UserProfileListener.php
+++ src/FOM/UserBundle/EventListener/UserProfileListener.php
@@ -3,10 +3,11 @@
 namespace FOM\UserBundle\EventListener;
 
 use Doctrine\Common\EventSubscriber;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
 use Doctrine\ORM\Id\AssignedGenerator;
-use Doctrine\DBAL\Platforms\MySqlPlatform;
 use Doctrine\DBAL\Platforms\OraclePlatform;
+use Doctrine\ORM\Mapping\ClassMetadata;
 
 /**
  * Event listener for adding user profile on the fly
@@ -17,13 +18,17 @@ class UserProfileListener implements EventSubscriber
 {
     /** @var string */
     protected $profileEntityName;
+    /** @var string */
+    protected $userEntityName;
 
     /**
-     * @param string $profileEntityName
+     * @param string $userEntityClass
+     * @param string $profileEntityClass
      */
-    public function __construct($profileEntityName)
+    public function __construct($userEntityClass, $profileEntityClass)
     {
-        $this->profileEntityName = $profileEntityName;
+        $this->userEntityName = $userEntityClass;
+        $this->profileEntityName = $profileEntityClass;
     }
 
     public function getSubscribedEvents()
@@ -36,43 +41,80 @@ class UserProfileListener implements EventSubscriber
     public function loadClassMetadata(LoadClassMetadataEventArgs $args)
     {
         $metadata = $args->getClassMetadata();
-        $user = 'FOM\UserBundle\Entity\User';
-        $basicProfile = 'FOM\UserBundle\Entity\BasicProfile';
-
-        if ($user == $metadata->getName()) {
-            $metadata->mapOneToOne(array(
-                'fieldName' => 'profile',
-                'targetEntity' => $this->profileEntityName,
-                'mappedBy' => 'uid',
-                'cascade' => array('persist'),
-            ));
+        $metadataClass = $metadata->getName();
+        if ($this->isUserEntity($metadataClass)) {
+            $this->patchUserEntity($metadata);
+        } elseif ($this->isProfileEntity($metadataClass)) {
+            $platform = $args->getEntityManager()->getConnection()->getDatabasePlatform();
+            $this->patchProfileEntity($metadata, $platform);
         }
+    }
 
-        // need to add metadata for the basic profile, else doctrine
-        // will whine in many situations
-        if ($this->profileEntityName == $metadata->getName() || $basicProfile == $metadata->getName()) {
-            $connection = $args->getEntityManager()->getConnection();
-            $platform = $connection->getDatabasePlatform();
-            $uidColname = $connection->quoteIdentifier('uid');
-            if ($platform instanceof OraclePlatform) {
-                $uidColname = strtoupper($uidColname);
-            } elseif ($platform instanceof MySqlPlatform) {
-                $uidColname = 'uid';
-            }
-            $metadata->setIdentifier(array('uid'));
-            $metadata->setIdGenerator(new AssignedGenerator());
-            $metadata->mapOneToOne(array(
-                'fieldName' => 'uid',
-                'targetEntity' => $user,
-                'inversedBy' => 'profile',
-                'id' => true,
-                'joinColumns' => array(
-                    array(
-                        'name' => $uidColname,
-                        'referencedColumnName' => 'id',
-                    ),
+    protected function patchUserEntity(ClassMetadata $metadata)
+    {
+        $metadata->mapOneToOne(array(
+            'fieldName' => 'profile',
+            'targetEntity' => $this->profileEntityName,
+            'mappedBy' => 'uid',
+            'cascade' => array('persist'),
+        ));
+    }
+
+    protected function patchProfileEntity(ClassMetadata $metadata, AbstractPlatform $platform)
+    {
+        /** @see https://www.doctrine-project.org/projects/doctrine-orm/en/2.6/reference/basic-mapping.html#quoting-reserved-words */
+        $uidColname = $platform->quoteIdentifier('uid');
+        if ($platform instanceof OraclePlatform) {
+            // why..?
+            $uidColname = strtoupper($uidColname);
+        }
+        $metadata->setIdentifier(array('uid'));
+        $metadata->setIdGenerator(new AssignedGenerator());
+        $metadata->mapOneToOne(array(
+            'fieldName' => 'uid',
+            'targetEntity' => $this->userEntityName,
+            'inversedBy' => 'profile',
+            'id' => true,
+            'joinColumns' => array(
+                array(
+                    'name' => $uidColname,
+                    'referencedColumnName' => 'id',
                 ),
-            ));
+            ),
+        ));
+    }
+
+    /**
+     * @param string $className
+     * @return boolean
+     */
+    protected function isUserEntity($className)
+    {
+        // this is checked for ALL entity types, so do as few instance checks as possible
+        $defaultClass = 'FOM\UserBundle\Entity\User';
+        if (\is_a($className, $defaultClass, true)) {
+            return true;
+        } elseif ($this->userEntityName !== $defaultClass) {
+            return \is_a($className, $this->userEntityName);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @param string $className
+     * @return boolean
+     */
+    protected function isProfileEntity($className)
+    {
+        // this is checked for ALL entity types, so do as few instance checks as possible
+        $defaultClass = 'FOM\UserBundle\Entity\BasicProfile';
+        if (\is_a($className, $defaultClass, true)) {
+            return true;
+        } elseif ($this->profileEntityName !== $defaultClass) {
+            return \is_a($className, $this->profileEntityName);
+        } else {
+            return false;
         }
     }
 }
