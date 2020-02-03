dfb54c08c1c56d053bc8e3eb34b6852b022a7e2a
werrolf <rolf.neuberger@wheregroup.com>
2020-02-03 11:41:44 +0100

    Split User entity in abstract base class and fancy, complex FOM user

--- src/FOM/UserBundle/Entity/User.php
+++ src/FOM/UserBundle/Entity/User.php
@@ -1,65 +1,36 @@
 <?php
+
+
 namespace FOM\UserBundle\Entity;
 
+
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\Common\Collections\ArrayCollection;
-use Symfony\Component\Security\Core\User\UserInterface;
+use FOM\UserBundle\EventListener\UserProfileListener;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
- * User entity.
- *
- * This needs enhancement, email should probably required. And we need a way
- * to implements user profiles which can vary from installation to
- * installation.
+ * User entity with email and (dynamic) association to a profile entity.
  *
  * @author Christian Wygoda
  * @author apour
  * @author Paul Schmidt
  *
  * @ORM\Entity
- * @UniqueEntity("username")
  * @UniqueEntity("email")
  * @ORM\Table(name="fom_user")
  */
-class User implements AdvancedUserInterface
+class User extends AbstractUser implements AdvancedUserInterface
 {
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    protected $id;
-
-    /**
-     * @ORM\Column(type="string", nullable=false, length=255, unique=true)
-     * @Assert\NotBlank()
-     * @Assert\Length(min=3)
-     */
-    protected $username;
-
     /**
      * @ORM\Column(type="string", nullable=false, length=255, unique=true)
      * @Assert\NotBlank()
      * @Assert\Email()
      */
     protected $email;
-
-    /**
-     * Password HASH, not verbatim password
-     * @var string|null
-     *
-     * @ORM\Column(type="string", nullable=false)
-     */
-    protected $password;
-
-    /**
-     * @ORM\Column(type="string", nullable=false)
-     */
-    protected $salt;
 
     /**
      * @ORM\Column(type="datetime", nullable=true)
@@ -88,63 +59,17 @@ class User implements AdvancedUserInterface
     protected $groups;
 
     /**
-     * The profile is not stored here, but a placeholder is needed
+     * Profile relation is initialized dynamically depending on config
+     * @see UserProfileListener::patchUserEntity()
      */
     protected $profile;
 
-    /**
-     *  Constructor
-     */
     public function __construct()
     {
         $this->groups = new ArrayCollection();
     }
 
     /**
-     * Set id
-     *
-     * @param integer $id
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set username
-     *
-     * @param string $username
-     * @return $this
-     */
-    public function setUsername($username)
-    {
-        $this->username = $username;
-        return $this;
-    }
-
-    /**
-     * Get username
-     *
-     * @return string
-     */
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
-    /**
-     * Set email
-     *
      * @param string $email
      * @return $this
      */
@@ -155,8 +80,6 @@ class User implements AdvancedUserInterface
     }
 
     /**
-     * Get email
-     *
      * @return string
      */
     public function getEmail()
@@ -165,51 +88,6 @@ class User implements AdvancedUserInterface
     }
 
     /**
-     * Set password
-     *
-     * @param string $password
-     * @return $this
-     */
-    public function setPassword($password)
-    {
-        $this->password = $password;
-        return $this;
-    }
-
-    /**
-     * Get password
-     *
-     * @return string
-     */
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    /**
-     * Set salt
-     *
-     * @param string $salt
-     */
-    public function setSalt($salt)
-    {
-        $this->salt = $salt;
-    }
-
-    /**
-     * Get salt
-     *
-     * @param string
-     * @return null|string
-     */
-    public function getSalt()
-    {
-        return $this->salt;
-    }
-
-    /**
-     * Set registrationTime
-     *
      * @param string $registrationTime
      */
     public function setRegistrationTime($registrationTime)
@@ -218,9 +96,7 @@ class User implements AdvancedUserInterface
     }
 
     /**
-     * Get registrationTime
-     *
-     * @return \DateTime
+     * @return \DateTime|null
      */
     public function getRegistrationTime()
     {
@@ -228,8 +104,6 @@ class User implements AdvancedUserInterface
     }
 
     /**
-     * Set registrationToken
-     *
      * @param string $registrationToken
      */
     public function setRegistrationToken($registrationToken)
@@ -238,8 +112,6 @@ class User implements AdvancedUserInterface
     }
 
     /**
-     * Get registrationToken
-     *
      * @return string
      */
     public function getRegistrationToken()
@@ -258,9 +130,7 @@ class User implements AdvancedUserInterface
     }
 
     /**
-     * Get resetTime
-     *
-     * @return \DateTime
+     * @return \DateTime|null
      */
     public function getResetTime()
     {
@@ -268,8 +138,6 @@ class User implements AdvancedUserInterface
     }
 
     /**
-     * Set resetToken
-     *
      * @param string $resetToken
      */
     public function setResetToken($resetToken)
@@ -291,18 +159,15 @@ class User implements AdvancedUserInterface
      * Add to group
      *
      * @param Group $group
-     * @return User
+     * @return $this
      */
     public function addGroup(Group $group)
     {
         $this->groups[] = $group;
-
         return $this;
     }
 
     /**
-     * Get groups
-     *
      * @return Collection|Group[]
      */
     public function getGroups()
@@ -311,8 +176,6 @@ class User implements AdvancedUserInterface
     }
 
     /**
-     * Get role objects
-     *
      * @return array
      */
     public function getRoles()
@@ -321,30 +184,8 @@ class User implements AdvancedUserInterface
         foreach ($this->getGroups() as $group) {
             $roles[] = $group->getRole();
         }
-        $roles[] = 'ROLE_USER';
+        $roles = array_merge($roles, parent::getRoles());
         return $roles;
-    }
-
-    /**
-     * Erase sensitive data like plain password. Don't fiddle with persisted data in here!
-     */
-    public function eraseCredentials()
-    {
-    }
-
-    /**
-     * Compare users
-     *
-     * This user class is only compatible with itself and compares the
-     * username property. If you'r needs differ, use a subclass.
-     *
-     * @param UserInterface $user The user to compare
-     * @return bool
-     */
-    public function equals(UserInterface $user)
-    {
-        return (get_class() === get_class($user)
-            && $this->getUsername() === $user->getUsername());
     }
 
     /**
@@ -414,38 +255,14 @@ class User implements AdvancedUserInterface
     }
 
     /**
-     * Remove groups
-     *
-     * @param Group $groups
-     */
-    public function removeGroup(Group $groups)
-    {
-        $this->groups->removeElement($groups);
-    }
-
-    /**
-     * @return bool
-     */
-    public function hasProfile()
-    {
-        return $this->getProfile() != null;
-    }
-
-    /**
-     * Checks whether the user is locked.
-     *
-     * Internally, if this method returns false, the authentication system
-     * will throw a LockedException and prevent login.
-     *
-     * @return bool true if the user is not locked, false otherwise
-     *
-     * @see LockedException
+     * @inheritDoc
      */
     public function isAccountNonLocked()
     {
         return true;
     }
-    
+
+    // why...?
     public function __toString()
     {
         return $this->getUsername() ?: '';
